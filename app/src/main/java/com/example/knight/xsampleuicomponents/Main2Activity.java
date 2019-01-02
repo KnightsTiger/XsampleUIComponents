c6fe5f87be3eaba0e7903f5473a59433a268d938
knight <tocaldera6734@gmail.com>
2019-01-02 16:39:54 +0530

    calenderview added

--- app/src/main/java/com/example/knight/xsampleuicomponents/Main2Activity.java
+++ app/src/main/java/com/example/knight/xsampleuicomponents/Main2Activity.java
@@ -2,12 +2,32 @@ package com.example.knight.xsampleuicomponents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView dateDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        calendarView = findViewById(R.id.calendarView);
+        dateDisplay =  findViewById(R.id.textView3);
+        dateDisplay.setText("Date: ");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                dateDisplay.setText("Date: " + i2 + " / " + (i1+1) + " / " + i);
+
+                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + (i1+1) + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
+            }
+        });
     }
-}
+
+    }
+
